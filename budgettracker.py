d449f762eae8b2bca4d7c990cb86d625de7d6c94
kbarry11 <156372857+kbarry11@users.noreply.github.com>
2024-04-19 12:06:41 -0400

    Continue building code

--- budgettracker.py
+++ budgettracker.py
@@ -23,10 +23,18 @@ def log_expense(transactions):
   save_transactions(transactions)
   print("Thank you. Your expense was logged successfully.")
 
-  #Function that logs your income
-  def log_income(transactions):
-    category = input("Please enter an income category: ")
-    amount = float(input("Now enter the income amount: "))
-    transactions["income"].append({"category": category, "amount": amount})
-    save_transactions(transactions)
-    print("Thank you. Your income was logged successfully.")
+#Function that logs your income
+def log_income(transactions):
+  category = input("Please enter an income category: ")
+  amount = float(input("Now enter the income amount: "))
+  transactions["income"].append({"category": category, "amount": amount})
+  save_transactions(transactions)
+  print("Thank you. Your income was logged successfully.")
+
+#Function that calculates total expenses
+def calculate_expenses(transactions):
+  return sum(item["amount"] for item in transactions["expenses"])
+    
+#Function that calculates income
+def calculate_income(transactions):
+  return sum(transactions["income"])
